5dc84e4822b1eb250aa98ff8d0f7c148c47cb13d
Nikolay Savchenko <107759407+NikolaySavchenko@users.noreply.github.com>
2022-09-06 16:07:48 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -10,12 +10,12 @@ def storage_information_view(request):
     in_storage_passcards = get_list_or_404(Visit.objects, leaved_at=None)
     non_closed_visits = []
     for in_storage_passcard in in_storage_passcards:
-        temp = {
+        non_closed_visit = {
             'who_entered': in_storage_passcard.passcard,
             'entered_at': localtime(in_storage_passcard.entered_at),
             'duration': format_duration(get_duration(in_storage_passcard))
         }
-        non_closed_visits.append(temp)
+        non_closed_visits.append(non_closed_visit)
 
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
