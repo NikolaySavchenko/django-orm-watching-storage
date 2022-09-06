3b48705f9ac85ff30046b64e99e667e5ba39d218
Nikolay Savchenko <107759407+NikolaySavchenko@users.noreply.github.com>
2022-09-06 16:03:28 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -11,13 +11,13 @@ from django.shortcuts import get_object_or_404
 
 def passcard_info_view(request, passcode):
     passcard_person = get_object_or_404(Passcard.objects, passcode=passcode)
-    all_visits_person = get_list_or_404(Visit.objects, passcard=passcard_person.id)
+    all_visits_persons = get_list_or_404(Visit.objects, passcard=passcard_person.id)
     this_passcard_visits = []
-    for i in range(len(all_visits_person)):
+    for all_visits_person in all_visits_persons:
         temp = {
-            'entered_at': localtime(all_visits_person[i].entered_at),
-            'duration': format_duration(get_duration(all_visits_person[i])),
-            'is_strange': is_visit_long(all_visits_person[i])
+            'entered_at': localtime(all_visits_person.entered_at),
+            'duration': format_duration(get_duration(all_visits_person)),
+            'is_strange': is_visit_long(all_visits_person)
         }
         this_passcard_visits.append(temp)
 
