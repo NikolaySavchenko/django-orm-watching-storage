f675a2e035baa1b9a1895b7656cdb3a87fcdfe21
Nikolay Savchenko <7548568@gmail.com>
2022-11-15 21:38:39 +0300

    Add default settings

--- project/settings.py
+++ project/settings.py
@@ -22,11 +22,11 @@ INTERNAL_IPS = ['127.0.0.1',]
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='localhost')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
